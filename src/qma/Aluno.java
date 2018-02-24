6bd4b4af0b0593b406bb4fd1b59b1b8079174164
Pigm3u <gabrielgtt@hotmail.com>
2018-02-24 15:03:18 -0300

    Mudancas em Pessoa, aluno e funcao. Adicionado toString de pessoa e assinatura de tornarTutor

--- src/qma/Aluno.java
+++ src/qma/Aluno.java
@@ -2,6 +2,6 @@ package qma;
 
 public class Aluno extends Funcao {
 	public Aluno(String nome, String matricula, int codigoCurso, String telefone, String email) {
-		super(nome, matricula, codigoCurso, telefone, email);
+		
 	}
 }
--- src/qma/Funcao.java
+++ src/qma/Funcao.java
@@ -1,14 +1,7 @@
 package qma;
 
 public class Funcao <Abstract> {
-	private String nome, matricula, telefone, email;
-	private int codigoCurso;
 	
-	public Funcao(String nome, String matricula, int codigoCurso, String telefone, String email) {
-		this.nome = nome;
-		this.matricula = matricula;
-		this.codigoCurso= codigoCurso;
-		this.telefone = telefone;
-		this.email = email;		
+	public Funcao() {
 	}
 }
