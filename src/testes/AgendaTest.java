f0115fb047ffa813ea472dc665c6a97210cff50a
Pigm3u <gabrielgtt@hotmail.com>
2018-02-27 18:10:31 -0300

    Finalizando Agenda e criando seus testes, além de gerar o HashCode e Equals de Aluno

--- src/testes/AgendaTest.java
+++ src/testes/AgendaTest.java
@@ -1,5 +1,57 @@
 package testes;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import qma.Agenda;
+
 public class AgendaTest {
 
+	private Agenda agenda;
+	
+	@Before
+	public void inicializar() {
+		this.agenda = new Agenda();
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void cadastroHorarioDiaEspacos() {
+		this.agenda.cadastrarHorarioDeAtendimento("15:00", "  ");
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void cadastroHorarioDiaVazio() {
+		this.agenda.cadastrarHorarioDeAtendimento("15:00", "");
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void cadastroHorarioHorarioEspacos() {
+		this.agenda.cadastrarHorarioDeAtendimento("   ", "seg");
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void cadastroHorarioHorarioVazio() {
+		this.agenda.cadastrarHorarioDeAtendimento("", "seg");
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void consultaHorarioDiaEspacos() {
+		this.agenda.consultaHorario("15:00", "  ");
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void consultaHorarioDiaVazio() {
+		this.agenda.consultaHorario("15:00", "");
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void consultaHorarioHorarioEspacos() {
+		this.agenda.consultaHorario("   ", "seg");
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void consultaHorarioHorarioVazio() {
+		this.agenda.consultaHorario("", "seg");
+	}
+	
 }
