97597834589cd69f99c6165acab23aedf8108acb
Matheus7OP <matheus7op@gmail.com>
2018-03-05 21:46:32 -0300

    Documentada classe Ajuda, documentada classe AjudaPresencial, adicionado testes de unidade de AjudaPresencial

--- src/qma/Ajuda.java
+++ src/qma/Ajuda.java
@@ -1,21 +1,55 @@
 package qma;
 
+/**
+ * Classe abstrata que representa uma
+ * ajuda, em sua forma mais geral, dentro
+ * do sistema.
+ * 
+ * @author Matheus Oliveira Pereira
+ * @author Gabriel Alves Tavares
+ */
 public abstract class Ajuda {
 	private String matriculaAluno, matriculaTutor;
 	private String disciplina;
 	
+	/**
+	 * Construtor do objeto Ajuda.
+	 * 
+	 * @param matriculaAluno a matricula do aluno que requisitou a ajuda
+	 * @param matriculaTutor a matricula do tutor que está responsável pela ajuda
+	 * @param disciplina a disciplina que será tutorada
+	 */
 	public Ajuda(String matriculaAluno, String matriculaTutor, String disciplina){
 		this.matriculaAluno = matriculaAluno;
 		this.matriculaTutor = matriculaTutor;
 		this.disciplina = disciplina;
 	}
 	
+	/**
+	 * Método utilizado para recuperar alguma informação
+	 * específica a respeito da ajuda
+	 * 
+	 * @param atributo o atributo desejado
+	 * @return o valor do atributo desejado
+	 */
 	public abstract String getInfoAjuda(String atributo);
 	
+	/**
+	 * Método utilizado para recuperar a
+	 * disciplina associada à ajuda.
+	 * 
+	 * @return a disciplina associada à ajuda
+	 */
 	public String getDisciplina() {
 		return this.disciplina;
 	}
 	
+	/**
+	 * Método utilizado para recuperar a matricula
+	 * do tutor que está associado à ajuda
+	 * 
+	 * @return a matricula do tutor associado
+	 */
 	public String pegarTutor() {
 		return this.matriculaTutor;
 	}
--- src/qma/AjudaPresencial.java
+++ src/qma/AjudaPresencial.java
@@ -1,9 +1,26 @@
 package qma;
 
+/**
+ * Classe que representa, no sistema,
+ * um pedido de ajuda do tipo presencial.
+ * 
+ * @author Matheus Oliveira Pereira
+ * @author Gabriel Alves Tavares
+ */
 public class AjudaPresencial extends Ajuda {
 	private String horario, dia;
 	private String localInteresse;
 	
+	/**
+	 * Construtor do objeto AjudaPresencial.
+	 * 
+	 * @param matriculaAluno a matricula do aluno que requer ajuda
+	 * @param matriculaTutor a matricula do tutor que irá fornecer ajuda
+	 * @param disciplina a disciplina que será tutorada
+	 * @param horario o provável horário em que ocorrerá o atendimento
+	 * @param dia o provável dia em que ocorrerá o atendimento
+	 * @param localInteresse o provável local em que ocorrerá o atendimento
+	 */
 	public AjudaPresencial(String matriculaAluno, String matriculaTutor, String disciplina, String horario, String dia, String localInteresse) {
 		super(matriculaAluno, matriculaTutor, disciplina);
 		this.horario = horario;
@@ -20,7 +37,7 @@ public class AjudaPresencial extends Ajuda {
 				return this.horario;
 			case "dia":
 				return this.dia;
-			case "localInteresse":
+			case "local":
 				return this.localInteresse;
 			default:
 				throw new IllegalArgumentException("Atributo inexistente!");
